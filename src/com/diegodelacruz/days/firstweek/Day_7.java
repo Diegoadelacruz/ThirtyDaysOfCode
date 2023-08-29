bd3985454b911dc622f81a4479cdbf5fd33f0e88
Diego de la Cruz <diego.dlacruz@gmail.com>
2023-08-29 11:42:05 -0600

    Day_6 completed

---
+++ src/com/diegodelacruz/days/firstweek/Day_7.java
@@ -0,0 +1,27 @@
+package com.diegodelacruz.days.firstweek;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given an array, A, of N integers, print A's elements
+ * in reverse order as a single line of space-separated numbers.
+ */
+public class Day_7 {
+
+    /**
+     * Example
+     * A = [1,2,3,4]
+     * Print 4,3,2,1
+     */
+    public static void main(String[] args) {
+        List<Integer> array = Arrays.asList(1, 4, 3, 2);
+        List<Integer> arr = new ArrayList<>(4);
+        arr.addAll(array);
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            System.out.print(arr.get(i) + " ");
+        }
+    }
+}
